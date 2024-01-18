0fb862a0f6f5723713351a812b0939fe20c36128
MichaelGloessl04 <gloess190117@gmail.com>
2024-01-18 22:10:47 +0100

    yeyeyeeyeye

--- py_book_importer/src/api.py
+++ py_book_importer/src/api.py
@@ -1,10 +1,21 @@
 import json
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from csv_importer import CSVImporter
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 def run():
     import uvicorn
